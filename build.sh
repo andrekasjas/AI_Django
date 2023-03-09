54fb69aed983eca93905bdf909933828309c871e
andrekasjas <andresmogollob@gmail.com>
2023-03-09 17:35:44 -0500

    v1

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
-#!/usr/bin/env bash
-# exit on error
 set -o errexit
 
 pip install -r requirements.txt
 python -m spacy download es_core_news_md
+
+python manage.py collectstatic --no-input
+python manage.py migrate
--- openai_app/models.py
+++ openai_app/models.py
@@ -1,7 +1,1 @@
 from django.db import models
-
-# Create your models here.
-class Message(models.Model):
-    text = models.CharField(max_length=1000)
-    sender = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
